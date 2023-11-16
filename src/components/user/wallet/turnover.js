23f5dddcb719f705f73515a6bb91f27f29b1c8ae
JavadH0sseini <sr.javadhosseini@gmail.com>
2023-11-16 14:02:29 +0330

    show a massage if checkouts or other arrays are empty

--- src/components/user/wallet/turnover.js
+++ src/components/user/wallet/turnover.js
@@ -154,95 +154,126 @@ const Turnover = () => {
             <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '20px' }}>
 
                 {
-                    (unpaidCheckouts && filterValue == 'Unpaid Checkouts') &&
-                    unpaidCheckouts.map(checkout => {
-                        return (
-                            <Card>
-                                <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <ArrowDown size={'50px'} color="white" />
-                                </Box>
-                                <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
-                                    <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
-                                        <Typography sx={{ color: '#F675A8', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Fail&nbsp;</Typography>
-                                        Charge Wallet
-                                    </Typography>
-                                    <Typography sx={{ color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
-                                        The attempt to charge your wallet with {checkout.tokens} YouWho tokens failed.
-                                    </Typography>
-                                    <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{checkout.iat.substring(0, 10)} {checkout.iat.substring(11, 16)}</Typography>
-                                </Box>
-                            </Card>
-                        )
-                    })
+                    filterValue == 'Unpaid Checkouts' &&
+                    <>{
+                        unpaidCheckouts.length ?
+                            unpaidCheckouts.map(checkout => {
+                                return (
+                                    <Card>
+                                        <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                            <ArrowDown size={'50px'} color="white" />
+                                        </Box>
+                                        <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
+                                            <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
+                                                <Typography sx={{ color: '#F675A8', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Fail&nbsp;</Typography>
+                                                Charge Wallet
+                                            </Typography>
+                                            <Typography sx={{ color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
+                                                The attempt to charge your wallet with {checkout.tokens} YouWho tokens failed.
+                                            </Typography>
+                                            <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{checkout.iat.substring(0, 10)} {checkout.iat.substring(11, 16)}</Typography>
+                                        </Box>
+                                    </Card>
+                                )
+                            }) : <Box sx={{ display: 'flex', flexDirection: 'column', height: '200px', alignItems: 'center', justifyContent: 'center' }}>
+                                {/* <EmptyWallet size={'50px'} color="#787878" /> */}
+                                <Typography sx={{ mt: 2, fontSize: '20px', color: 'primary.darkGray', textAlign: 'center', mb: 2, fontWeight: '500' }}>
+                                    There are no Unpaid Checkouts
+                                </Typography>
+                            </Box>
+                    }</>
                 }
                 {
-                    (paidCheckouts && filterValue == 'Paid Checkouts') &&
-                    paidCheckouts.map(checkout => {
-                        return (
-                            <Card>
-                                <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <ArrowDown size={'50px'} color="white" />
-                                </Box>
-                                <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
-                                    <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
-                                        <Typography sx={{ color: '#3DCA64', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Succcessfull&nbsp;</Typography>
-                                        Charge Wallet
-                                    </Typography>
-                                    <Typography sx={{ color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
-                                        Your wallet has been charged with {checkout.tokens} YouWho-tokens!
-                                    </Typography>
-                                    <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{checkout.iat.substring(0, 10)} {checkout.iat.substring(11, 16)}</Typography>
-                                </Box>
-                            </Card>
-                        )
-                    })
+                    filterValue == 'Paid Checkouts' &&
+                    <>{
+                        paidCheckouts.length ?
+                            paidCheckouts.map(checkout => {
+                                return (
+                                    <Card>
+                                        <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                            <ArrowDown size={'50px'} color="white" />
+                                        </Box>
+                                        <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
+                                            <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
+                                                <Typography sx={{ color: '#3DCA64', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Succcessfull&nbsp;</Typography>
+                                                Charge Wallet
+                                            </Typography>
+                                            <Typography sx={{ color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
+                                                Your wallet has been charged with {checkout.tokens} YouWho-tokens!
+                                            </Typography>
+                                            <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{checkout.iat.substring(0, 10)} {checkout.iat.substring(11, 16)}</Typography>
+                                        </Box>
+                                    </Card>
+                                )
+                            })
+                            : <Box sx={{ display: 'flex', flexDirection: 'column', height: '200px', alignItems: 'center', justifyContent: 'center' }}>
+                                <Typography sx={{ mt: 2, fontSize: '20px', color: 'primary.darkGray', textAlign: 'center', mb: 2, fontWeight: '500' }}>
+                                    There are no Paid Checkouts
+                                </Typography>
+                            </Box>
+                    }</>
                 }
                 {
-                    (userDeposits && filterValue == 'Deposits') &&
-                    userDeposits.map(deposite => {
-                        return (
-                            <Card>
-                                <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <Icon url={GiftIcon} w={50} h={50} />
-                                </Box>
-                                <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
-                                    <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
-                                        <Typography sx={{ color: '#3DCA64', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Succcessfull&nbsp;</Typography>
-                                        Deposite NFT Gift
-                                    </Typography>
-                                    <Typography sx={{ display:'flex',color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
-                                        You have successfully sent {deposite.amount} YouWho tokens to
-                                        <Typography sx={{ width: '5em', overflow: 'hidden', fontFamily: 'Inter', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontSize: '14px', fontWeight: '400', lineHeight: '24px', display: 'inline-block' }}>
-                                            &nbsp;{deposite.recipient_screen_cid}
-                                        </Typography>
-                                        !
-                                    </Typography>
-                                    <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{deposite.iat.substring(0, 10)} {deposite.iat.substring(11, 16)}</Typography>
-                                </Box>
-                            </Card>
-                        )
-                    })
+                    filterValue == 'Deposits' &&
+                    <>{
+                        userDeposits.length ?
+                            userDeposits.map(deposite => {
+                                return (
+                                    <Card>
+                                        <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                            <Icon url={GiftIcon} w={50} h={50} />
+                                        </Box>
+                                        <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
+                                            <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
+                                                <Typography sx={{ color: '#3DCA64', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Succcessfull&nbsp;</Typography>
+                                                Deposite NFT Gift
+                                            </Typography>
+                                            <Typography sx={{ display: 'flex', color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
+                                                You have successfully sent {deposite.amount} YouWho tokens to
+                                                <Typography sx={{ width: '5em', overflow: 'hidden', fontFamily: 'Inter', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontSize: '14px', fontWeight: '400', lineHeight: '24px', display: 'inline-block' }}>
+                                                    &nbsp;{deposite.recipient_screen_cid}
+                                                </Typography>
+                                                !
+                                            </Typography>
+                                            <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{deposite.iat.substring(0, 10)} {deposite.iat.substring(11, 16)}</Typography>
+                                        </Box>
+                                    </Card>
+                                )
+                            }) : <Box sx={{ display: 'flex', flexDirection: 'column', height: '200px', alignItems: 'center', justifyContent: 'center' }}>
+                                <Typography sx={{ mt: 2, fontSize: '20px', color: 'primary.darkGray', textAlign: 'center', mb: 2, fontWeight: '500' }}>
+                                    There are no Deposits
+                                </Typography>
+                            </Box>
+                    }</>
                 }
                 {
-                    (userWithdrawals && filterValue == 'Withdrawals') &&
-                    userWithdrawals.map(withdrawal => {
-                        return (
-                            <Card>
-                                <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <Icon url={GiftOpenIcon} w={50} h={50} />
-                                </Box>
-                                <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
-                                    <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
-                                        <Typography sx={{ color: '#3DCA64', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Succcessfull&nbsp;</Typography>
-                                        Claim NFT Gift
-                                    </Typography>
-                                    <Typography sx={{ color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
-                                        You successfully claimed YouWho-tokens!                                        </Typography>
-                                    <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{withdrawal.wat.substring(0, 10)} {withdrawal.wat.substring(11, 16)}</Typography>
-                                </Box>
-                            </Card>
-                        )
-                    })
+                    filterValue == 'Withdrawals' &&
+                    <>{
+                        userWithdrawals.length ?
+                            userWithdrawals.map(withdrawal => {
+                                return (
+                                    <Card>
+                                        <Box sx={{ width: '84px', height: '84px', borderRadius: '18px', backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                            <Icon url={GiftOpenIcon} w={50} h={50} />
+                                        </Box>
+                                        <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '3px' }}>
+                                            <Typography sx={{ color: 'primary.text', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px' }}>
+                                                <Typography sx={{ color: '#3DCA64', fontFamily: 'inter', fontSize: '16px', fontWeight: '500', lineHeight: '26px', display: 'inline-block' }}>Succcessfull&nbsp;</Typography>
+                                                Claim NFT Gift
+                                            </Typography>
+                                            <Typography sx={{ color: 'primary.text', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontFamily: 'Inter', fontSize: '14px', fontWeight: '400', lineHeight: '24px' }}>
+                                                You successfully claimed YouWho-tokens!                                        </Typography>
+                                            <Typography sx={{ color: 'primary.darkGray', fontFamily: 'inter', fontSize: '12px', fontWeight: '400', lineHeight: '24px' }}>{withdrawal.wat.substring(0, 10)} {withdrawal.wat.substring(11, 16)}</Typography>
+                                        </Box>
+                                    </Card>
+                                )
+                            }) :
+                            <Box sx={{ display: 'flex', flexDirection: 'column', height: '200px', alignItems: 'center', justifyContent: 'center' }}>
+                                <Typography sx={{ mt: 2, fontSize: '20px', color: 'primary.darkGray', textAlign: 'center', mb: 2, fontWeight: '500' }}>
+                                    There are no Withdrawals
+                                </Typography>
+                            </Box>
+                    }</>
                 }
             </Box>
         </Box >
